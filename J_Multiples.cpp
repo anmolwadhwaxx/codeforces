cc70669ca345479aee1d4fb4b7e2f002c557b976
Anmol Wadhwa <94642300+anmolwadhwaxx@users.noreply.github.com>
2022-10-27 23:52:46 +0530

    First Commit
    First Commit into the Codeforces

---
+++ J_Multiples.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{int a,b=0;
+cin>>a>>b;
+if((a%b==0)||(b%a==0))
+{cout<<"multiples";}
+else{cout<<"no multiples";}
+   return 0; 
+}
+
+Question - https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/J
